2df151f9ca8dfb8374eff37340a2840dfacc02da
namancg <naman.c@perfios.com>
2021-09-16 19:59:41 +0530

    REFACTORED: interfaces added

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.hotelreservationsystem;
 
+import java.time.LocalDate;
+
 public interface HotelReservationIF {
 	public void addHotel(String hotelName, double regularCustomerRate);
 	public int getHotelListSize();
 	public void printHotelList();
+	public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate);
 }
