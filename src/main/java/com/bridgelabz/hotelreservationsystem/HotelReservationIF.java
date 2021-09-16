da438addca66a0d363898ca2dad7a58161dbc34d
namancg <naman.c@perfios.com>
2021-09-16 21:07:18 +0530

    REFACTORED: changes return type string for cheapHotel

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
@@ -7,6 +7,6 @@ public interface HotelReservationIF {
 	public void addHotel(String hotelName, double weekdayCustomerCost, double weekendCustomerCost);
 	public int getHotelListSize();
 	public void printHotelList();
-	public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate);
+	public String getCheapestHotel(LocalDate startDate, LocalDate endDate);
 	public ArrayList<Hotel> getHotelList();
 }
