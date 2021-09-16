fe16ecc6b30d8602842ef1d90229e4281cf748f6
namancg <naman.c@perfios.com>
2021-09-16 21:14:32 +0530

    REFACTORED: added rating parameter

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public interface HotelReservationIF {
-	public void addHotel(String hotelName, double weekdayCustomerCost, double weekendCustomerCost);
+	public void addHotel(String hotelName, double weekdayCustomerCost, double weekendCustomerCost, int rating);
 	public int getHotelListSize();
 	public void printHotelList();
 	public String getCheapestHotel(LocalDate startDate, LocalDate endDate);
