275d5cb45698f2ba223eae9a8eaf98394959cd1d
namancg <naman.c@perfios.com>
2021-09-16 21:13:54 +0530

    REFACTORED: ability to add ratings

--- src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
@@ -5,11 +5,13 @@ public class Hotel {
 	private double regularCustomerCost;
 	double weekdayCustomerCost;
 	double weekendCustomerCost;
+	private int rating;
 	public Hotel() {
 	}
 	public Hotel(String hotelName, int rating, double regularCustomerCost) {
 		this.hotelName = hotelName;
 		this.regularCustomerCost = regularCustomerCost;
+		this.rating=rating;
 	}
 	public String getHotelName() {
 		return hotelName;
@@ -39,8 +41,15 @@ public class Hotel {
 	public void setWeekendCustomerCost(double weekendCustomerCost) {
 		this.weekendCustomerCost = weekendCustomerCost;
 	}
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
 	@Override
 	public String toString() {
-		return "hotelName=" + hotelName + "Customer WeekDay Cost="+ weekdayCustomerCost+ "Customer WeekDay Cost="+ weekdayCustomerCost;
+		return "hotelName=" + hotelName + "Customer WeekDay Cost="+ weekdayCustomerCost+ "Customer WeekDay Cost="+ weekdayCustomerCost+"Rating="+rating;
 	}
 }
