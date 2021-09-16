6f00fe33df89290163dd29397466d8d77659cb5e
namancg <naman.c@perfios.com>
2021-09-16 19:40:52 +0530

    ADDED: welcome message

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystemMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelReservationSystemMain {
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO HOTEL RESERVATION SYSTEM");
+
+	}
+
+}
