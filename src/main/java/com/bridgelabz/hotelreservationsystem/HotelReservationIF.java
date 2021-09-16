79a096ba146e64b520573181627f83e8bbec7655
namancg <naman.c@perfios.com>
2021-09-16 20:17:33 +0530

    REFACTORED: added parameters in addHotel method

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
@@ -3,7 +3,7 @@ package com.bridgelabz.hotelreservationsystem;
 import java.time.LocalDate;
 
 public interface HotelReservationIF {
-	public void addHotel(String hotelName, double regularCustomerRate);
+	public void addHotel(String hotelName, double weekdayCustomerCost, double weekendCustomerCost);
 	public int getHotelListSize();
 	public void printHotelList();
 	public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate);
