cb1f327308e18036f91877c0b71fc9abf0f83425
namancg <naman.c@perfios.com>
2021-09-17 19:01:21 +0530

    REFACTORED: parameters in function

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
@@ -7,10 +7,10 @@ public interface HotelReservationIF {
 	public void addHotel(String hotelName, double weekdayCustomerCost, double weekendCustomerCost, int rating, double weekdayRewardCost, double weekendRewardCost);
 	public int getHotelListSize();
 	public void printHotelList();
-	public ArrayList<Hotel> getCheapestHotel(LocalDate startDate, LocalDate endDate);
+	public ArrayList<Hotel> getCheapestHotel(LocalDate startDate, LocalDate endDate, String customerType);
 	public ArrayList<Hotel> getHotelList();
-	public Hotel getCheapestBestRatedHotel(LocalDate startDate, LocalDate endDate);
-	public Hotel getBestRatedHotel(LocalDate startDate, LocalDate endDate);
+	public Hotel getCheapestBestRatedHotel(LocalDate startDate, LocalDate endDate, String customerType);
+	public Hotel getBestRatedHotel(LocalDate startDate, LocalDate endDate, String customerType);
 	public String getDates();
 	public boolean validateDate(String date);
 }
